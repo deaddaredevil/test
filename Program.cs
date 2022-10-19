409361a05b030d8678ac26f4be88933d7502ce91
deaddaredevil <113775017+deaddaredevil@users.noreply.github.com>
2022-10-19 20:33:34 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace test
+{
+    class Program
+    {
+        static void Main()
+        {
+            int standartTime;
+            float workTime;
+            double velueWork;
+            float completedWork;
+            string employeeName;
+            int rework;
+            float bonus;
+            float salaryCount;
+            double taxation;
+            float prepayment;
+            float amountToIssue;
+        }
+    }
+}
